c9d516971f26c283096c331ec449cfb9e17e20b7
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-12 11:46:41 -0300

    getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
+  const { meetup } = props;
+  const { image, title, description, address } = meetup;
+
   return (
     <MeetupDetail
-      image="https://cdn.pixabay.com/photo/2014/01/13/04/17/meetup-243206_960_720.jpg"
-      title="A First Meetup"
-      description="The meetup description"
-      address="Some Street 5, Some City"
+      image={image}
+      title={title}
+      description={description}
+      address={address}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { meetupId } = context.params;
+
+  return {
+    props: {
+      meetup: {
+        id: meetupId,
+        image:
+          'https://cdn.pixabay.com/photo/2014/01/13/04/17/meetup-243206_960_720.jpg',
+        title: 'A First Meetup',
+        description: 'The meetup description',
+        address: 'Some Street 5, Some City',
+      },
+    },
+    revalidate: 3600,
+  };
 }
 
 export default MeetupDetails;
